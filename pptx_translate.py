980e28bd089702d2e314a4714ff521991b20807b
Marco Wang <marco060314@gmail.com>
2025-07-11 12:51:57 -0700

    Update pptx_translate.py

--- pptx_translate.py
+++ pptx_translate.py
@@ -73,4 +73,4 @@ if __name__ == "__main__":
         api_key = f.read().strip()
 
     translator = PowerPointTranslator(api_key)
-    translator.translate_pptx("smart3.pptx", "translated_smart3.pptx")
+    translator.translate_pptx("90deg.pptx", "translated_90deg.pptx")
